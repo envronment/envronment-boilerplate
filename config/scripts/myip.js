497d7ea1ca98d8c6c9fb428217075284f7613cd5
taltk9 <taltk9@gmail.com>
2017-09-28 13:33:03 -0300

    Add script to print local IP

--- config/scripts/myip.js
+++ config/scripts/myip.js
@@ -1,7 +1,3 @@
-// ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
-
-// ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
-
 const shell = require('shelljs');
 const ip = require('ip');
 
@@ -13,9 +9,3 @@ const command = [
 ].join(' | ');
 
 shell.exec(command);
-
-// shell.exec('ifconfig', function(code, stdout, stderr) {
-//   console.log('Exit code:', code);
-//   console.log('Program output:', stdout);
-//   console.log('Program stderr:', stderr);
-// });
